dd5b4fabf9f3d290579743acb41c3139a5d6b39d
Sudipta Dey <sdptd20@gmail.com>
2024-05-14 15:33:08 +0530

    add logo to the sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@nextui-org/react";
+import Image from 'next/image'
 
 const Sidebar: React.FC = () => {
     return (
         <div className="fixed md:relative bottom-0 left-0 md:top-0 md:left-0 w-full md:w-24 shadow-xl text-white flex md:flex-col p-4 space-x-4 md:space-x-0 md:space-y-4">
             <div className="h-full w-full flex justify-between">
                 <div className="md:flex-col flex gap-4">
+                    <Image className="hidden md:block mx-auto" src='/logo.png' width={70} height={100} alt='logo' />
                     <Button isIconOnly aria-label="Add Task">
                         <i className="bi bi-plus-lg"></i>
                     </Button>
