e70d328dd3de1994b58a0135c4af5b4157ababb4
Sudipta Dey <sdptd20@gmail.com>
2024-05-14 20:13:38 +0530

    multiDelete and clear filter

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,14 +1,16 @@
 import { Button } from "@nextui-org/react";
 import Image from 'next/image'
+import { Dispatch, SetStateAction } from "react";
 
 import { ModalTypes } from "../helpers/enums";
 
 interface SidebarProps {
     setModalType: (modalType: ModalTypes) => void,
-    onOpen: () => void
+    onOpen: () => void,
+    setMultiDeleteActive: Dispatch<SetStateAction<boolean>>
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ setModalType, onOpen }) => {
+const Sidebar: React.FC<SidebarProps> = ({ setModalType, onOpen, setMultiDeleteActive }) => {
     return (
         <div className="fixed md:relative bottom-0 left-0 md:top-0 md:left-0 w-full md:w-24 shadow-xl text-white flex md:flex-col p-4 space-x-4 md:space-x-0 md:space-y-4">
             <div className="h-full w-full flex justify-center md:justify-normal">
@@ -20,7 +22,7 @@ const Sidebar: React.FC<SidebarProps> = ({ setModalType, onOpen }) => {
                     <Button isIconOnly aria-label="Add Task">
                         <i className="bi bi-plus-lg"></i>
                     </Button>
-                    <Button isIconOnly color="warning" aria-label="Delete Tasks">
+                    <Button isIconOnly color="warning" aria-label="Delete Tasks" onClick={() => { setMultiDeleteActive((prevData: boolean) => !prevData) }}>
                         <i className="bi bi-trash"></i>
                     </Button>
                 </div>
--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import { useDisclosure } from "@nextui-org/react";
+import { Checkbox, CheckboxGroup, Divider, useDisclosure } from "@nextui-org/react";
 import { useState } from "react";
 
 import ModalComponenet from "../../components/ModalComponent";
@@ -26,14 +26,6 @@ const cardData: CardData[] = [
     { id: 2, title: 'Card 2', description: 'This is a slightly longer description that will increase the card height.', onClick: () => alert('Card 2 clicked') },
     { id: 3, title: 'Card 3', description: 'This description is very long. It will make the card significantly taller than the other cards in the grid, demonstrating the masonry layout effect.', onClick: () => alert('Card 3 clicked') },
     { id: 4, title: 'Card 4', description: 'Description 4.', onClick: () => alert('Card 4 clicked') },
-    { id: 1, title: 'Card 1', description: 'This is a short description.', onClick: () => alert('Card 1 clicked') },
-    { id: 2, title: 'Card 2', description: 'This is a slightly longer description that will increase the card height.', onClick: () => alert('Card 2 clicked') },
-    { id: 3, title: 'Card 3', description: 'This description is very long. It will make the card significantly taller than the other cards in the grid, demonstrating the masonry layout effect.', onClick: () => alert('Card 3 clicked') },
-    { id: 4, title: 'Card 4', description: 'Description 4.', onClick: () => alert('Card 4 clicked') },
-    { id: 1, title: 'Card 1', description: 'This is a short description.', onClick: () => alert('Card 1 clicked') },
-    { id: 2, title: 'Card 2', description: 'This is a slightly longer description that will increase the card height.', onClick: () => alert('Card 2 clicked') },
-    { id: 3, title: 'Card 3', description: 'This description is very long. It will make the card significantly taller than the other cards in the grid, demonstrating the masonry layout effect.This description is very long. It will make the card significantly taller than the other cards in the grid, demonstrating the masonry layout effect.This description is very long. It will make the card significantly taller than the other cards in the grid, demonstrating the masonry layout effect.', onClick: () => alert('Card 3 clicked') },
-    { id: 4, title: 'Card 4', description: 'Description 4.', onClick: () => alert('Card 4 clicked') },
     // Add more cards as needed
     // Add more cards as needed
 ];
@@ -41,6 +33,8 @@ const cardData: CardData[] = [
 const App: React.FC = () => {
     const [selectedTab, setSelectedTab] = useState(TaskStatus.All);
     const [modalType, setModalType] = useState(ModalTypes.Filter);
+    const [selectedTaskCards, setSelectedTaskCards] = useState<string[]>([]);
+    const [multiDeleteActive, setMultiDeleteActive] = useState(false);
     const [filterData, setFilterData] = useState<FilterValues>({
         startDate: null,
         endDate: null,
@@ -52,27 +46,53 @@ const App: React.FC = () => {
 
     return (
         <div className="flex h-[90%] md:h-full">
-            <Sidebar onOpen={onOpen} setModalType={setModalType} />
+            <Sidebar onOpen={onOpen} setModalType={setModalType} setMultiDeleteActive={setMultiDeleteActive} />
             <div className="p-4 flex flex-col flex-grow">
                 <Topbar taskStatus={TaskStatus.All} />
                 <TaskStatusTabs selectedKey={selectedTab} setSelectedKey={setSelectedTab} />
                 {isAnyFilterActive &&
-                    <div className=" py-2 px-2">
-                        <p className="hover:cursor-pointer hover:underline w-fit text-danger" onClick={() => setFilterData({ startDate: null, endDate: null, sortOption: null, sortOrder: null })}>Clear Filter</p>
+                    <div className="flex space-x-2 py-2 px-2 items-center w-full overflow-hidden">
+                        <p className="hover:cursor-pointer hover:underline w-fit text-danger whitespace-nowrap" onClick={() => setFilterData({ startDate: null, endDate: null, sortOption: null, sortOrder: null })}>Clear Filter</p>
+                        <Divider className="my-4 w-full" />
                     </div>
                 }
-                <div className="flex-col flex-grow overflow-y-auto py-4 px-1">
-                    <div className="columns-2 md:columns-3 xl:columns-5 gap-4">
-                        {cardData.map((card) => (
-                            <div key={card.id} className="mb-4 break-inside-avoid">
-                                <TaskCard
-                                    title={card.title}
-                                    description={card.description}
-                                    onClick={card.onClick}
-                                />
-                            </div>
-                        ))}
+                {multiDeleteActive && (
+                    <div className="flex space-x-4 py-2 px-2 items-center overflow-hidden">
+                        <p className="whitespace-nowrap">{selectedTaskCards.length} tasks selected</p>
+                        <p className="hover:cursor-pointer hover:underline w-fit text-danger whitespace-nowrap" onClick={() => null}>Delete Cards</p>
+                        <p className="hover:cursor-pointer hover:underline w-fit" onClick={() => { setMultiDeleteActive(false); setSelectedTaskCards([]); }}>Cancel</p>
+                        <Divider className="my-4 w-full" />
                     </div>
+                )}
+                <div className="flex-col flex-grow overflow-y-auto py-4 px-1">
+                    <CheckboxGroup
+                        label="Select cities"
+                        defaultValue={selectedTaskCards}
+                        onValueChange={setSelectedTaskCards}
+                    >
+                        <div className="columns-2 md:columns-3 xl:columns-5 gap-4">
+                            {cardData.map((card) => (
+                                <>
+                                    {multiDeleteActive ?
+                                        <Checkbox value={card.id.toString()} key={card.id} className="mb-4 break-inside-avoid">
+                                            <TaskCard
+                                                title={card.title}
+                                                description={card.description}
+                                                onClick={card.onClick}
+                                            />
+                                        </Checkbox > :
+                                        <TaskCard
+                                            title={card.title}
+                                            description={card.description}
+                                            onClick={card.onClick}
+                                        />
+                                    }
+                                </>
+
+                            ))}
+                        </div>
+                    </CheckboxGroup>
+
                 </div>
             </div>
             <ModalComponenet type={modalType}
