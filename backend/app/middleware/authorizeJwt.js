8e7e5b07a018294f412091c33444da7fb4f1531b
Sudipta Dey <sdptd20@gmail.com>
2024-05-16 00:05:05 +0530

    fix verifyToken logic

--- backend/app/middleware/authorizeJwt.js
+++ backend/app/middleware/authorizeJwt.js
@@ -2,11 +2,21 @@ const jwt = require('jsonwebtoken');
 const config = require('../config/auth.config.js');
 
 function verifyToken(request, response, next) {
-    let token = request.headers['x-access-token'];
-    if (!token)
+    let authHeader = request.headers['authorization'];
+    if (!authHeader) {
         return response.status(403).send({ error: 'No token provided' });
+    }
+
+    // Extract the token from the 'Bearer <token>' format
+    let token = authHeader.split(' ')[1];
+    if (!token) {
+        return response.status(403).send({ error: 'Invalid token format' });
+    }
+
     jwt.verify(token, config.secret, (error, decoded) => {
-        if (error) return response.status(401).send({ error: 'Unauthorized' });
+        if (error) {
+            return response.status(401).send({ error: 'Unauthorized' });
+        }
         request.userId = decoded.id;
         next();
     });
