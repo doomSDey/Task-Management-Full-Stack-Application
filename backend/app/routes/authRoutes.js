4a5196eb97b7225c2743616dcece24a20c93c187
Sudipta Dey <sdptd20@gmail.com>
2024-05-15 16:02:56 +0530

    add swagger doc

--- backend/app/routes/authRoutes.js
+++ backend/app/routes/authRoutes.js
@@ -1,6 +1,130 @@
 const { verifySignUp } = require('../middleware');
 const authController = require('../controllers').auth;
 
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: Authentication management
+ */
+
+/**
+ * @swagger
+ *  /auth/signup:
+ *   post:
+ *     summary: Sign up a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - username
+ *               - email
+ *               - password
+ *               - avatarId
+ *             properties:
+ *               username:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               avatarId:
+ *                 type: string
+ *             example:
+ *               username: johndoe
+ *               email: johndoe@example.com
+ *               password: secret123
+ *               avatarId: avatar123
+ *     responses:
+ *       201:
+ *         description: The user was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *
+ * @swagger
+ * /auth/signin:
+ *   post:
+ *     summary: Sign in a user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             example:
+ *               email: john@example.com
+ *               password: secret123
+ *     responses:
+ *       200:
+ *         description: The user was successfully signed in
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 username:
+ *                   type: string
+ *                 email:
+ *                   type: string
+ *                 avatarId:
+ *                   type: string
+ *                 accessToken:
+ *                   type: string
+ *       401:
+ *         description: Invalid username or password
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ */
+
 module.exports = (app) => {
     // Auth headers //
     app.use((request, response, next) => {
@@ -11,6 +135,7 @@ module.exports = (app) => {
         next();
     });
     // Auth routes //
+
     app.post(
         '/signup',
         [verifySignUp.checkForDuplicateEmail],
